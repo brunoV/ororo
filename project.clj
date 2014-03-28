143217870acda9d96f3b0971df32baff7d12d62b
Bruno Vecchi <vecchi.b@gmail.com>
2014-03-27 23:39:55 -0300

    Replace clj-http with httpkit
    All functions now return a promise instead of the actual response.

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "A Wunderground API library."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/data.json "0.1.2"]
-                 [clj-http "0.2.1"]])
+                 [clj-http "0.2.1"]
+                 [http-kit "2.1.18"]])
